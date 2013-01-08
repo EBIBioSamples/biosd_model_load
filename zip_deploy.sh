83c237d5c75deef9748aaaa2d41bc4f5eaf0de16
Marco Brandizi <brandizi@ebi.ac.uk>
2013-01-08 10:00:29 +0000

    Changing the POM to make it Java-6. Minor bugfixing in deploying scripts.

--- zip_deploy.sh
+++ zip_deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#ÊDeploys the command line binary. This doesn't do much, but it's needed with Bamboo.
-#Ê
+# Deploys the command line binary. This doesn't do much, but it's needed with Bamboo.
+# 
 
 MYDIR=$(dirname "$0")
 cd "$MYDIR"/target
@@ -9,14 +9,14 @@ cd "$MYDIR"/target
 target="$1"
 
 if [ "$target" == "" ]; then
-  target=target
+  target=.
 fi;
 
 echo
 echo 
 echo _______________ Deploying the Command Line Binary to $target _________________
 
-yes A| unzip biosd_loader_shell_*.zip -d /ebi/microarray/home/biosamples-dev/loader_relational/dev
+yes A| unzip biosd_loader_shell_*.zip -d "$target"
 
 echo ______________________________________________________________________________
 echo
