df037a5197f535eedc707644a4e511dbdff42878
Marco Brandizi <brandizi@ebi.ac.uk>
2013-03-13 13:53:43 +0000

    Some minimal commenting added, more to come.

--- src/main/java/ac/uk/ebi/fg/biosd/sampletab/parser/object_normalization/Normalizer.java
+++ src/main/java/ac/uk/ebi/fg/biosd/sampletab/parser/object_normalization/Normalizer.java
@@ -3,11 +3,21 @@
  */
 package ac.uk.ebi.fg.biosd.sampletab.parser.object_normalization;
 
+import com.google.common.collect.Table;
+
+import uk.ac.ebi.arrayexpress2.sampletab.datamodel.MSI;
 import uk.ac.ebi.fg.core_model.toplevel.Identifiable;
 
 
 /**
- * TODO: Comment me!
+ * Object normalisers are meant to take an instance of the BioSD object model and merge those objects that have to be
+ * reused. This can be done in-memory, typically after having parsed a SampleTab submission, where duplicates created by
+ * the underlining Limpopo-based parser are unified, or it can be done just before saving a {@link MSI a SampleTab Submission}
+ * into a relational database, where objects that already exist in the DB replace the ones in memory (created from scratch).
+ * 
+ * The two type of normalisation are achieved using the same normaliser components (i.e., the subclasses of this class)
+ * by simply using two different {@link Store} implementations, {@link MemoryStore one} that keeps/lookup objects in a 
+ * {@link Table double-key hash table} and the {@link DBStore other one} that lookup existing objects into a relational database.  
  *
  * <dl><dt>date</dt><dd>Mar 11, 2013</dd></dl>
  * @author Marco Brandizi
--- src/main/java/ac/uk/ebi/fg/biosd/sampletab/parser/object_normalization/Store.java
+++ src/main/java/ac/uk/ebi/fg/biosd/sampletab/parser/object_normalization/Store.java
@@ -3,6 +3,9 @@
  */
 package ac.uk.ebi.fg.biosd.sampletab.parser.object_normalization;
 
+import uk.ac.ebi.fg.core_model.toplevel.Accessible;
+import uk.ac.ebi.fg.core_model.xref.ReferenceSource;
+
 
 /**
  * TODO: Comment me!
@@ -13,5 +16,15 @@ package ac.uk.ebi.fg.biosd.sampletab.parser.object_normalization;
  */
 public interface Store
 {
+	/**
+	 * targetIds are a set of strings that should be used to search for the existence of newObject (i.e., represent its
+	 * search key). For example this may be a single string for an {@link Accessible}, two strings for an accession + version 
+	 * when a {@link ReferenceSource} is being searched.
+	 * 
+	 * The result must return null if the new object was not found inside the store. The implementation can choose whether
+	 * to save or not the new object before returning such null result. Of course the result is non-null when an object
+	 * that has the same search key already exists in the store (in particular it can be newObject itself). 
+	 *  
+	 */
 	public <T> T find ( T newObject, String... targetIds ); 
 }
