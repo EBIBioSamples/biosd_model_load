46170a937c70a942ce47e7b63a1a910c94dc5078
Marco Brandizi <brandizi@ebi.ac.uk>
2014-01-16 10:59:38 +0000

    Putting auto-fetched version in the deployment script.

--- zip_deploy.sh
+++ zip_deploy.sh
@@ -4,6 +4,11 @@
 # 
 
 MYDIR=$(dirname "$0")
+
+cd "$MYDIR"
+vcmd='mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version --offline'
+version=$($vcmd | grep -Ei '^[0-9]+[a-z,+,-,\.,_]')
+
 cd "$MYDIR"/target
 
 target="$1"
@@ -17,7 +22,7 @@ echo
 echo _______________ Deploying the Command Line Binary to $target _________________
 
 yes A| unzip biosd_loader_shell_*.zip -d "$target"
-chmod -R ug=rwX,o=rX "$target/biosd_loader_shell_*"
+chmod -R ug=rwX,o=rX "$target/biosd_loader_shell_$version"
 
 echo ______________________________________________________________________________
 echo
