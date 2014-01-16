61d7a8d327351ca4161f31d2c0da8005406ed648
Marco Brandizi <brandizi@ebi.ac.uk>
2014-01-16 15:53:39 +0000

    Going back to previous deployment script (version fetching doesn't work with release process).

--- zip_deploy.sh
+++ zip_deploy.sh
@@ -3,22 +3,7 @@
 # Deploys the command line binary. This doesn't do much, but it's needed with Bamboo.
 # 
 
-# Needed under EBI/Bamboo environment
-
-if [ "$USER" == 'ma-svc' ]; then
- export JAVA_HOME=/ebi/research/software/Linux_x86_64/opt/java/jdk1.6.0_31
- export PATH="$PATH:$JAVA_HOME/bin"
-
- export M2_HOME=/ebi/microarray/home/bamboo/maven-3.0.4
- export PATH="$PATH:$M2_HOME/bin" 
-fi
-
 MYDIR=$(dirname "$0")
-
-cd "$MYDIR"
-vcmd='mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version'
-version=$($vcmd | grep -Ei '^[0-9]+[a-z,+,-,\.,_]')
-
 cd "$MYDIR"/target
 
 target="$1"
@@ -32,7 +17,7 @@ echo
 echo _______________ Deploying the Command Line Binary to $target _________________
 
 yes A| unzip biosd_loader_shell_*.zip -d "$target"
-chmod -R ug=rwX,o=rX "$target/biosd_loader_shell_$version"
+chmod -R ug=rwX,o=rX "$target/biosd_loader_shell_*"
 
 echo ______________________________________________________________________________
 echo
