8e593f102193ad5cd0716caa470e128239af639a
Marco Brandizi <brandizi@ebi.ac.uk>
2015-09-04 16:27:28 +0100

    package.sh improved.

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-mvn clean package -DargLine="-Xms512m -Xmx2G -XX:PermSize=128m -XX:MaxPermSize=1G" \
-    -Dsurefire.useFile=true \
-    -Ptest.h2,h2_file # see pom.xml for other profiles
+mvn clean package $MVN_BUILD_OPTS -DargLine="-Xms512m -Xmx2G -XX:PermSize=128m -XX:MaxPermSize=1G" \
+    -Dsurefire.useFile=true
+
 
